b4b0ddec45ad225c165c2a4787b512bbcad6b61f
VasiliyTemniy <flash_er@mail.ru>
2023-10-05 18:53:59 +0300

    add server creation, connection and simple usage test

---
+++ src/tests/server_test.go
@@ -0,0 +1,50 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"m-cafe-auth/src/server"
+	"testing"
+
+	pb "m-cafe-auth/src/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func TestMain(t *testing.T) {
+	flushDB()
+
+	// Run server in background
+	go func() {
+		server.RunServer()
+	}()
+
+	// Send gRPC message to that server
+	conn, err := grpc.Dial("[::1]:4006", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	// Create gRPC client
+	client := pb.NewAuthServiceClient(conn)
+
+	// Create request
+	req := &pb.AuthRequest{
+		Id:         123,
+		LookupHash: "test",
+		Password:   "test",
+	}
+
+	// Send request
+	res, err := client.CreateAuth(context.Background(), req)
+	if err != nil {
+		t.Fatalf("could not greet: %v", err)
+	}
+
+	if res.Id != 123 {
+		t.Errorf("expected id: %s, got: %s", "123", fmt.Sprintf("%v", (res.Id)))
+	}
+}
