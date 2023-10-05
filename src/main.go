fe680f63ccc6173b8005866660423cec2f54dae1
VasiliyTemniy <flash_er@mail.ru>
2023-10-05 18:52:46 +0300

    move RunServer() to server.go for tests

--- src/main.go
+++ src/main.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
-
-	"m-cafe-auth/src/configs"
-	pb "m-cafe-auth/src/proto"
-	"m-cafe-auth/src/services"
-
-	"google.golang.org/grpc"
+	"m-cafe-auth/src/server"
 )
 
 func main() {
-	listenPort := fmt.Sprintf("[::1]:%s", configs.EnvPort)
-
-	lis, err := net.Listen("tcp", listenPort)
-
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	} else {
-		fmt.Println("Listening on " + listenPort)
-	}
-
-	grpcServer := grpc.NewServer()
-	service := &services.AuthServiceServer{}
-
-	pb.RegisterAuthServiceServer(grpcServer, service)
-	err = grpcServer.Serve(lis)
-
-	if err != nil {
-		log.Fatalf("Error strating server: %v", err)
-	}
+	server.RunServer()
 }
