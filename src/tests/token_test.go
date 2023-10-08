5456957fef1b620d4b0f4927878446a19933f102
VasiliyTemniy <flash_er@mail.ru>
2023-10-08 18:33:06 +0300

    upd token creation and verification test

--- src/tests/token_test.go
+++ src/tests/token_test.go
@@ -1,9 +1,13 @@
 package tests
 
 import (
+	"crypto/x509"
 	"fmt"
+	"simple-micro-auth/src/cert"
 	s "simple-micro-auth/src/services"
 	"testing"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var (
@@ -11,50 +15,38 @@ var (
 )
 
 const (
-	validExpectedToken         = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzX2F0IjoxMDAwMDAwMDAwMDAsImlkIjoxMjMsInJhbmQiOjEyMzQ1fQ.LdolfV3z2h-4b-M7fYNAktz0UU2RtuS0CD2UdRK_iog"
-	invalidToken               = "eyJhbGciOiJIUzI1NiIsF0IjoxMDAwMDAwMDAwMDAsImlkIjoxMjMsInJhbmQiOjEyMzQ1fQ.V5z1mWBH9LrMo49AFMT-CYpuAEFQ"
-	invalidIdClaimToken        = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzX2F0IjoxMDAwMDAwMDAwMDAsImlkIjoiMTIzIiwicmFuZCI6MTIzNDV9.KPTmtGzVG2ED4Z5cgTQuhdC9Wt-Y3F4aQJXlGuPNeYo"
-	invalidExpiresAtClaimToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzX2F0IjoiMTAwMDAwMDAwMDAwIiwiaWQiOjEyMywicmFuZCI6MTIzNDV9.1_t6KnWT8ds8B4Bg86FmYOXxMHoJxi_LWqchQrdKK8o"
-	invalidSignatureToken      = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzX2F0IjoxMDAwMDAwMDAwMDAsImlkIjoxMjMsInJhbmQiOjEyMzQ1fQ.55lgZVHQ0Vy-uPWcgov19yUeJQd6sEcUsrb1HnLLF-E"
-	invalidMethodToken         = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJqb3BhIjoiMTMyIiwiZXhwaXJlc19hdCI6MTAwMDAwMDAwMDAwLCJpZCI6MTIzLCJyYW5kIjoxMjM0NX0.6Ei6AP9rDqtPF5AhKOmg7ZDcpA_amX4azbHGPZX-N0ZU4RV5746SsvK9pK-7HdxW"
-	mockExpiresAt              = int64(100000000000)
-	mockEnvJWTSecret           = "JustATestSecret"
-	mockRandomNumber           = int64(12345)
-	mockId                     = int64(123)
+	mockExpiresAt    = int64(100000000000)
+	mockTimeNow      = int64(99990)
+	mockRandomNumber = int64(12345)
+	mockId           = int64(123)
 )
 
-func TestCreateToken(t *testing.T) {
-	// Test case 1: Valid token should be created
+func init() {
+	cert.ReadCertificates("token")
+}
 
-	tokenString, err := tokenHandler.CreateToken(mockId, mockExpiresAt, mockRandomNumber, mockEnvJWTSecret)
+func TestCreateAndVerifyToken(t *testing.T) {
+
+	// Test case 1: Valid token should be created
+	validTokenString, err := tokenHandler.CreateToken(mockId, mockTimeNow, mockExpiresAt, mockRandomNumber, cert.PrivateKey)
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
 	}
 
-	if tokenString != validExpectedToken {
-		t.Errorf("expected token: %s, got: %s", validExpectedToken, tokenString)
-	}
-}
-
-func TestVerifyToken(t *testing.T) {
-	// Test case 1: Valid token, not expired
-	token := validExpectedToken
-	mockTimeNow := mockExpiresAt - 1000
-	expectedID := mockId
-
-	id, err := tokenHandler.VerifyToken(token, mockTimeNow, mockEnvJWTSecret)
+	id, err := tokenHandler.VerifyToken(validTokenString, mockTimeNow, cert.PublicKey)
 	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
+		t.Errorf("unexpected error: %v", err)
 	}
-	if id != expectedID {
-		t.Errorf("Expected ID: %d, got: %d", expectedID, id)
+
+	if id != mockId {
+		t.Errorf("Expected ID: %d, got: %d", mockId, id)
 	}
 
 	// Test case 2: Invalid token
-	token = invalidToken
+	invalidToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzX2F0IjoxMDAwMDAwMDAwMDAsImlkIjoxMjMsInJhbmQiOjEyMzQ1fQ.55lgZVHQ0Vy-uPWcgov19yUeJQd6sEcUsrb1HnLLF-E"
 	expectedErr := fmt.Errorf("invalid token or signature")
 
-	id, err = tokenHandler.VerifyToken(token, mockTimeNow, mockEnvJWTSecret)
+	id, err = tokenHandler.VerifyToken(invalidToken, mockTimeNow, cert.PublicKey)
 	if err.Error() != expectedErr.Error() {
 		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
 	}
@@ -62,13 +54,12 @@ func TestVerifyToken(t *testing.T) {
 		t.Errorf("Expected ID: 0, got: %d", id)
 	}
 
-	// Test case 3: Expired token
-	token = validExpectedToken
+	// Test case 3: expired token
 	expectedErr = fmt.Errorf("token expired")
 
-	timeNow := mockExpiresAt + 1000
+	muchMoreMockTimeNow := mockExpiresAt + 1000
 
-	id, err = tokenHandler.VerifyToken(token, timeNow, mockEnvJWTSecret)
+	id, err = tokenHandler.VerifyToken(validTokenString, muchMoreMockTimeNow, cert.PublicKey)
 	if err.Error() != expectedErr.Error() {
 		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
 	}
@@ -76,23 +67,51 @@ func TestVerifyToken(t *testing.T) {
 		t.Errorf("Expected ID: 0, got: %d", id)
 	}
 
-	// Test case 4: Unexpected signing method
-	token = invalidMethodToken
+	// Test case 4: unexpected signing method
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(cert.PublicKey)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	invalidSignedToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":   id,
+		"rand": mockRandomNumber,
+		"exp":  mockExpiresAt,
+		"iat":  mockTimeNow,
+		"nbf":  mockTimeNow,
+		"iss":  "simple-micro-auth",
+	}).SignedString(publicKeyBytes)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	expectedButInterceptedErr := fmt.Errorf("invalid token or signature")
 	expectedErr = fmt.Errorf("unexpected signing method")
 
-	id, err = tokenHandler.VerifyToken(token, mockTimeNow, mockEnvJWTSecret)
-	if err.Error() != expectedErr.Error() {
+	id, err = tokenHandler.VerifyToken(invalidSignedToken, mockTimeNow, cert.PublicKey)
+	if err.Error() != expectedErr.Error() && err.Error() != expectedButInterceptedErr.Error() {
 		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
 	}
 	if id != 0 {
 		t.Errorf("Expected ID: 0, got: %d", id)
 	}
 
-	// Test case 5: Malformed exp claim
-	token = invalidExpiresAtClaimToken
+	// Test case 5: malformed exp claim
+	invalidExpClaimToken, err := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"id":   id,
+		"rand": mockRandomNumber,
+		// "exp":  "invalid",
+		"iat": mockTimeNow,
+		"nbf": mockTimeNow,
+		"iss": "simple-micro-auth",
+	}).SignedString(cert.PrivateKey)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
 	expectedErr = fmt.Errorf("exp claim malformed")
 
-	id, err = tokenHandler.VerifyToken(token, mockTimeNow, mockEnvJWTSecret)
+	id, err = tokenHandler.VerifyToken(invalidExpClaimToken, mockTimeNow, cert.PublicKey)
 	if err.Error() != expectedErr.Error() {
 		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
 	}
@@ -100,25 +119,22 @@ func TestVerifyToken(t *testing.T) {
 		t.Errorf("Expected ID: 0, got: %d", id)
 	}
 
-	// Test case 6: Malformed id claim
-	token = invalidIdClaimToken
+	// Test case 6: malformed id claim
+	invalidIdClaimToken, err := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"id":   "invalid",
+		"rand": mockRandomNumber,
+		"exp":  mockExpiresAt,
+		"iat":  mockTimeNow,
+		"nbf":  mockTimeNow,
+		"iss":  "simple-micro-auth",
+	}).SignedString(cert.PrivateKey)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
 	expectedErr = fmt.Errorf("id claim malformed")
 
-	id, err = tokenHandler.VerifyToken(token, mockTimeNow, mockEnvJWTSecret)
-	if err.Error() != expectedErr.Error() {
-		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
-	}
-	if id != 0 {
-		t.Errorf("Expected ID: 0, got: %d", id)
-	}
-
-	// Test case 7: Invalid signature
-	token = validExpectedToken
-	expectedErr = fmt.Errorf("invalid token or signature")
-
-	secret := "invalid"
-
-	id, err = tokenHandler.VerifyToken(token, mockTimeNow, secret)
+	id, err = tokenHandler.VerifyToken(invalidIdClaimToken, mockTimeNow, cert.PublicKey)
 	if err.Error() != expectedErr.Error() {
 		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
 	}
